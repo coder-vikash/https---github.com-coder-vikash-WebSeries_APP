886c1c902852127ae5d50245ec3ec7387fa5b0f9
coder-vikash <vk422821@gmail.com>
2025-07-21 02:06:06 +0530

    slider

--- projects/src/Components/HeroSection/HeroClosur.jsx
+++ projects/src/Components/HeroSection/HeroClosur.jsx
@@ -2,17 +2,25 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
+import "./Closour.css"; // custom CSS
 
 const movies = [
   {
     id: 1,
     title: "Stranger Things",
+    subtitle: "Supernatural | Drama | 50m",
+    description:
+      "A group of young friends witness supernatural forces and secret government exploits.",
     image:
       "https://images.filmibeat.com/img/2025/05/saruserialreview-1747326591.jpg",
+    link: "https://www.netflix.com/in/title/80057281",
   },
   {
     id: 2,
     title: "Money Heist",
+    subtitle: "Crime | Thriller | 1h",
+    description:
+      "A mastermind and his team of thieves pull off the biggest heist in recorded history.",
     image:
       "https://m.media-amazon.com/images/M/MV5BMmNkMWZmNWQtNjljNi00YTU4LTg0ZTEtY2U0OTk0MmI3NTNmXkEyXkFqcGc@._V1_.jpg",
     link: "https://www.netflix.com/in/title/80192098",
@@ -20,58 +28,64 @@ const movies = [
   {
     id: 3,
     title: "The Family Man",
+    subtitle: "Action | Drama | 45m",
+    description:
+      "A middle-class man secretly works as an intelligence officer while balancing his personal life.",
     image:
       "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRozXRE4oMmMybGPC4WQkMHMmU8j0Qsg9HYJQCozi1Cg3Mha7j5oI-TliQhDeQR460I3ih87Q",
     link: "https://www.primevideo.com/detail/The-Family-Man/0KRGHG8WZ8RZ2STP1MIE8ZW2KP",
   },
   {
     id: 4,
-    title: "The Boys",
+    title: "The Family Man",
+    subtitle: "Action | Drama | 45m",
+    description:
+      "A middle-class man secretly works as an intelligence officer while balancing his personal life.",
     image:
-      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRYxan0RVMaKr1JxY2uZlvhPqhqU-KIGXbYpU_LW1HG3-f9-RpYQlOPok90hExi-L3cvRuE",
-    link: "https://www.primevideo.com/detail/0LTVQJYJ2GD3W64X6BIORWEMUR/",
+      "https://cdn.dribbble.com/userupload/31794716/file/original-8cd9068c1ed8e73cf9834fc4012f7a8c.jpg?crop=111x0-1136x769&format=webp&resize=400x300&vertical=center",
+    link: "https://www.primevideo.com/detail/The-Family-Man/0KRGHG8WZ8RZ2STP1MIE8ZW2KP",
   },
   {
     id: 5,
-    title: "Sacred Games",
+    title: "The Family Man",
+    subtitle: "Action | Drama | 45m",
+    description:
+      "A middle-class man secretly works as an intelligence officer while balancing his personal life.",
     image:
-      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQn5iQVUMmCBKWoRRAjY4qORrvL2SgxW8dSESz9BuRTIuB0L8AGgw1SkXNEGRxc6upBTSRpPA",
-    link: "https://www.netflix.com/in/title/80115328",
+      "https://docs.madrasthemes.com/vodi/wp-content/uploads/sites/11/2019/02/movies-slider-style-1-output.jpg",
+    link: "https://www.primevideo.com/detail/The-Family-Man/0KRGHG8WZ8RZ2STP1MIE8ZW2KP",
   },
 ];
 
-function MovieCarousel() {
+function HeroClosour() {
   return (
-    <Carousel fade pause="hover" interval={3000}>
+    <Carousel fade pause="hover" interval={4000} className="movie-carousel">
       {movies.map((movie) => (
         <Carousel.Item key={movie.id}>
-          <img
-            className="d-block w-100"
-            src={movie.image}
-            alt={movie.title}
-            style={{
-              height: "500px",
-              objectFit: "cover",
-              backgroundImage: "cover",
-
-              borderRadius: "5px",
-            }}
-          />
-          <Carousel.Caption>
-            <h3 className="text-white fw-bold">{movie.title}</h3>
-            <Button
-              variant="danger"
-              href={movie.link}
-              target="_blank"
-              className="mt-2"
-            >
-              CineStream
-            </Button>
-          </Carousel.Caption>
+          <div
+            className="carousel-image"
+            style={{ backgroundImage: `url(${movie.image})` }}
+          >
+            <div className="carousel-overlay">
+              <div className="carousel-content container text-start text-light">
+                <h1 className="display-4 fw-bold mb-2">{movie.title}</h1>
+                <p className="lead fst-italic mb-1">{movie.subtitle}</p>
+                <p className="mb-4">{movie.description}</p>
+                <Button
+                  variant="danger"
+                  href={movie.link}
+                  target="_blank"
+                  className="px-4 py-2"
+                >
+                  ▶ Watch Now
+                </Button>
+              </div>
+            </div>
+          </div>
         </Carousel.Item>
       ))}
     </Carousel>
   );
 }
 
-export default MovieCarousel;
+export default HeroClosour;
